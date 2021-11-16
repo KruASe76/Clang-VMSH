3cf62d7a6a2dc347a2c4c5bd0c2a089957aa4d2e
KruASe <55000336+KruASe76@users.noreply.github.com>
2021-11-16 23:56:09 +0300

    Add files via upload

---
+++ homework/for 10.11.21/task2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main() {
+    char str1[1000], str2[1000];
+    printf("Input first string:\n");
+    scanf("%1000s", str1);
+    printf("\nInput second string:\n");
+    scanf("%1000s", str2);
+
+    for (int i = 0; i < 1000; i++) {
+        if (str1[i] != str2[i]) {
+            printf("\nStrings are not similar\nCharacters matched: %d\n", i);
+            break;
+        } else {
+            if ((int)str1[i] == 0) {
+                printf("\nStrings are similar\nAll characters (%d) mathced\n", i);
+                break;
+            }
+        }
+    }
+}
