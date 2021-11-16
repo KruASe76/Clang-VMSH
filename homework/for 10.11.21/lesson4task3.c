ac0df1310649488d5ac11f72a4d4e23f2859fff2
KruASe <55000336+KruASe76@users.noreply.github.com>
2021-11-17 02:06:50 +0300

    Add files via upload

---
+++ homework/for 10.11.21/lesson4task3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main() {
+    char arr[16][17] = {{" "}}; // 17 из-за терминатора
+    int n;
+
+    printf("Input 16 numbes, each from 0 to 16:\n");
+    for (int i = 0; i < 16; i++) {
+        scanf("%d", &n);
+
+        if (n < 0 || n > 16) {
+            printf("Warning: the number has to be from 0 to 16\n");
+        } else {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = (char) 42; // "*" symbol
+                // У меня также проблемы с заменой чаров в строках, если указывать в кавычках, траблы с кодировкой...
+            }
+        }
+    }
+
+    printf("\n");
+    for (int i = 0; i < 16; i++) {
+        printf("%s\n", arr[i]);
+    }
+}
